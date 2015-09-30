f9dce19cb632f54e7431eba4bcda37f41bba539a
Mangoooo <mangoer08@gmail.com>
2015-09-30 19:12:54 +0800

    put down artist image

--- gameoriginal/Scripts/Game.js
+++ gameoriginal/Scripts/Game.js
@@ -8,9 +8,13 @@ theGame.Game = function(game)
 	this.red = null; // gem
 	this.blue = null;
 	this.green = null;
+	this.yellowGem = null;
+	this.cyanGem = null;
 	this.redcustomer = null; 
 	this.bluecustomer = null;
 	this.greencustomer = null;
+	this.yellowcustomer = null;
+	this.cyancustomer = null;
     this.counter = 100;
 
 	this.sprite = null;
@@ -21,68 +25,109 @@ theGame.Game = function(game)
     this.WaitToDisappear_bool_Timer = null;
 	this.setToKill = null;
 	this.countDown = 10;
+	
 	this.money = 0;
 	this.moneyText = null;
 	this.moneyImage = null;
+	this.coinImage = null;
+	this.moneyskin = null;
+	
 	this.CusNum = 0;
 	this.CusNumText = null;
 	
 	this.dude = null;
 	this.dude2 = null;
 	this.dude3 = null;
+	this.dude4 = null;
 	
 	this.greenCorrect = false;
 	this.redCorrect = false;
 	this.blueCorrect = false;
+	this.iconImage = null;
 	
 	this.middle = false;
 	this.randomCus = null;
 	this.customerArray = [false, false, false];
 	this.totalCustomers = 0;
+	
+	this.bubble = null;
+	this.tableImage = null;
 
 	this.timer = 20;
 	this.timerText = 0;
-	this.clockImage;
-	this.angle = { min: 0, max: 0 };
+	
+	this.clockImage = null;
+	this.clockSkin = null;
+	this.angle = 360;
 };
 
 theGame.Game.prototype = 
 {
+	
+//////////////////////////////////////Preload////////////////////////////////////////
     preload: function()
     {
         //Screen Background
         this.gameBackground = this.add.sprite(this.world.width*0.5, this.world.height*0.5, 'GameBackGround');
         this.gameBackground.anchor.set(0.5,0.5);
 		
-		// gem 
-	
-        this.red = this.add.sprite(this.world.width*0.4, this.world.height*0.7, 'red');
-        this.red.anchor.set(0.5,0.5);
+		//gemTable
+		this.tableImage = this.add.sprite(this.world.width*0.5, this.world.height*0.85, 'gemTable');
+        this.tableImage.anchor.set(0.5,0.5);
 		
+		// gem 
+        this.red = this.add.sprite(this.world.width*0.3, this.world.height*0.7, 'red');
+        this.red.anchor.set(0.5,0.5);	
    		this.red.inputEnabled = true;
 		this.red.events.onInputDown.add(this.redclick, this);
 
-		this.blue = this.add.sprite(this.world.width*0.5, this.world.height*0.7, 'blue');
+		this.blue = this.add.sprite(this.world.width*0.4, this.world.height*0.8, 'blue'); // 0.4 0.8
         this.blue.anchor.set(0.5,0.5);
-		
 		this.blue.inputEnabled = true;
 		this.blue.events.onInputDown.add(this.blueclick, this);
 		
-		this.green = this.add.sprite(this.world.width*0.6, this.world.height*0.7, 'green');
+		this.green = this.add.sprite(this.world.width*0.7, this.world.height*0.7, 'green'); // 0.5 0.7
         this.green.anchor.set(0.5,0.5);
-		
 		this.green.inputEnabled = true;
 		this.green.events.onInputDown.add(this.greenclick, this);
 		
-		this.moneyImage = this.add.sprite(this.world.width*0.7, this.world.height*0.7, 'moneyImage');
-		this.moneyImage.anchor.set(0.5,0.5);
-		this.moneyImage.scale.setTo(1, 1);
-
-//timerText
-        this.timerText = this.add.text(this.world.centerX, this.world.centerY, 'Timer: 20', { font: "40px Arial", fill: "#000000", align: "center" });
-    	this.timerText.anchor.setTo(0.5, 0.5);
+		this.yellowGem = this.add.sprite(this.world.width*0.6, this.world.height*0.8, 'yellow'); //0.6 0.8
+        this.yellowGem.anchor.set(0.5,0.5);
+		this.yellowGem.inputEnabled = true;
+		this.yellowGem.events.onInputDown.add(this.yellowclick, this);
+		
+		this.cyanGem = this.add.sprite(this.world.width*0.5, this.world.height*0.7, 'cyan'); //0.7 0.7
+        this.cyanGem.anchor.set(0.5,0.5);
+		this.cyanGem.inputEnabled = true;
+		this.cyanGem.events.onInputDown.add(this.cyanclick, this);
+		
+		//Money
+//		this.moneyImage = this.add.sprite(this.world.width*0.8, this.world.height*0.7, 'moneyImage');
+//		this.moneyImage.anchor.set(0.5,0.5);
+//		this.moneyImage.scale.setTo(1, 1);
+		
+		//customer icon
+		this.iconImage = this.add.sprite(this.world.width*0.45, this.world.height*0.08, 'customerIcon');
+        this.iconImage.anchor.set(0.5,0.5);
+		
+		//clock
+		this.clockSkin = this.add.sprite(this.world.width*0.08, this.world.height*0.85, 'clockskin');
+        this.clockSkin.anchor.set(0.5,0.5);
+		this.clockSkin.scale.setTo(0.8,0.8);
+		
+		this.clockImage = this.add.sprite(this.world.width*0.08, this.world.height*0.85, 'clock');
+        this.clockImage.anchor.set(0.5,0.5);
+		this.clockImage.scale.setTo(0.8,0.8);
+		
+		//money jar
+		this.moneyskin = this.add.sprite(this.world.width*0.93, this.world.height*0.85, 'moneyjar');
+        this.moneyskin.anchor.set(0.5,0.5);
+		
+		//timerText
+        //this.timerText = this.add.text(this.world.centerX, this.world.centerY, 'Timer: 20', { font: "40px Arial", fill: "#000000", align: "center" });
+    	//this.timerText.anchor.setTo(0.5, 0.5);
 		this.moneyText = this.add.text(600, 50, 'Money: 0', { font:"30px Arial", fill: "#000", align: "center"});
-		this.CusNumText = this.add.text(200, 50, 'Customer: 0', { font:"30px Arial", fill: "#000", align: "center"});
+		this.CusNumText = this.add.text(460, 40, ': 0', { font:"30px Arial", fill: "#000", align: "center"});
     }, 
        
     create: function()
@@ -91,32 +136,37 @@ theGame.Game.prototype =
 
 		this.spawnCustomer();
 
-		this.clockImage = this.add.graphics(0,0); // draw clock 
-		this.add.tween(this.angle).to({max: 359 }, 20000, "Linear", true); //10000   = 10		
+		this.clockImage = this.add.tween(this.clockImage).to( { angle: 360 }, 20000, Phaser.Easing.Linear.None, true);  //10
     },
 	
 	update: function()
     { 
 		//console.log(this.greenCorrect);
 		//console.log(this.dude.body.velocity.x);
-		//this.randomCusFunc(this.randomCus);e
+		this.randomCusFunc(this.randomCus);
 		
 		if(this.money >= 10 && this.money <= 19)
 		{
-			this.moneyImage.frame = 1;
+			//this.moneyImage.frame = 1;
+			this.coinImage = this.add.sprite(this.world.width*0.93, this.world.height*0.92, 'coin');
+			this.coinImage.anchor.set(0.5,0.5);
 		}
 		else if(this.money >= 20 && this.money <= 29)
 		{
-			this.moneyImage.frame = 2;
+			//this.moneyImage.frame = 2;
+			this.coinImage = this.add.sprite(this.world.width*0.91, this.world.height*0.91, 'coin');
+			this.coinImage.anchor.set(0.5,0.5);
 
 		}
 		else if(this.money >= 30 && this.money <= 39)
 		{
-			this.moneyImage.frame = 3;
+			//this.moneyImage.frame = 3;
+			this.coinImage = this.add.sprite(this.world.width*0.95, this.world.height*0.90, 'coin');
+			this.coinImage.anchor.set(0.5,0.5);
 		}
 		
 		
-		if (this.dude != null)
+		if (this.dude != null)  
 		{
 			this.blueCustomerFunc();
 		}
@@ -129,13 +179,8 @@ theGame.Game.prototype =
 			this.redCustomerFunc();
 		}
 		
-		this.clockImage.clear();
-		this.clockImage.lineStyle(2, 0xa000f3);
-		this.clockImage.beginFill(928325);
-		this.clockImage.arc(200, 500, 80, this.angle.min, this.math.degToRad(this.angle.max), true); //x positon, y position, scale 
-		this.clockImage.endFill();
+		this.clockImage.angle += 1;
     },
-	
 	
 	spawnCustomer: function()
 	{
@@ -154,6 +199,17 @@ theGame.Game.prototype =
 				this.dude.animations.add('left', [0,1,2,3]);     	
 				this.dude.animations.add('right', [5,6,7,8]);
 				this.dude.animations.add('still', [4]); 
+				
+				this.physics.startSystem(Phaser.Physics.ARCADE);
+				this.dude4 = this.add.sprite(this.world.width*0.15, this.world.height*0.3, 'dude4');
+				this.dude4.anchor.set(0.5,0.5);
+				this.dude4.scale.setTo(2, 2);
+				this.physics.enable(this.dude4, Phaser.Physics.ARCADE);
+				this.dude4.body.velocity.setTo(100,0);
+
+				this.dude4.animations.add('left', [0,1,2,3]);     	
+				this.dude4.animations.add('right', [5,6,7,8]);
+				this.dude4.animations.add('still', [4]);		
 			}
 			else if(this.randomCus == 2)
 			{	
@@ -179,7 +235,7 @@ theGame.Game.prototype =
 
 				this.dude3.animations.add('left', [0,1,2,3]);     	
 				this.dude3.animations.add('right', [5,6,7,8]);
-				this.dude3.animations.add('still', [4]);			
+				this.dude3.animations.add('still', [4]);		
 			}  
 			this.totalCustomers++;
 		}
@@ -190,7 +246,8 @@ theGame.Game.prototype =
 		do
 		{
 			this.randomCus = this.game.rnd.integerInRange(Min,Max);
-		} while (this.customerArray[this.randomCus - 1] == true)
+		} 
+		while (this.customerArray[this.randomCus - 1] == true)
 	},
 	
 	randomCusFunc: function(randomCus)
@@ -218,21 +275,33 @@ theGame.Game.prototype =
 		{
 			this.dude.animations.play('right',10, true);
 			this.dude.body.velocity.setTo(100, 0);
+			this.dude4.animations.play('right',10, true);
+			this.dude4.body.velocity.setTo(100, 0);
 		}
 		else if(this.dude.x > this.world.centerX+5 && this.middle == false)
 		{
 			this.dude.animations.play('right',10, true);
 			this.dude.body.velocity.setTo(100, 0);
+			this.dude4.animations.play('right',10, true);
+			this.dude4.body.velocity.setTo(100, 0);
 		}
 		else
 		{
 			this.dude.body.velocity.setTo(0, 0);
 			this.dude.animations.play('still',10, true);
+			this.dude4.body.velocity.setTo(0, 0);
+			this.dude4.animations.play('still',10, true);
 			this.middle = true;
+		}
+		if(this.middle == true)
+		{
+			this.bubble = this.add.sprite(this.world.width*0.2, this.world.height*0.2, 'request');
+			
 		}
 		if(this.blueCorrect == true)
 		{
 			this.dude.x += 0.5;
+			this.dude4.x += 0.5;
 			this.middle = false;
 		}
 	},
@@ -244,11 +313,13 @@ theGame.Game.prototype =
 		{
 			this.dude2.animations.play('right',10, true);
 			this.dude2.body.velocity.setTo(100, 0);
+			this.middle = false;
 		}
 		else if(this.dude2.x > this.world.centerX+5 && this.middle == false)
 		{
 			this.dude2.animations.play('right',10, true);
 			this.dude2.body.velocity.setTo(100, 0);
+			this.middle = false;
 		}
 		else
 		{
@@ -256,13 +327,25 @@ theGame.Game.prototype =
 			this.dude2.animations.play('still',10, true);
 			this.middle = true;	
 		}
+		
+		if(this.middle == true)
+		{
+			this.bubble = this.add.sprite(this.world.width*0.2, this.world.height*0.2, 'request');
+			console.log(this.middle);
+			
+			if( this.middle == false)
+			{
+				//this.destroyBubble();
+			}
+		}
 		if(this.greenCorrect == true)
 		{
 			this.dude2.x +=0.5;
 			this.middle = false;
+			
 		}
 	},
-	
+		
 	redCustomerFunc: function()
 	{
 		this.redcustomer = 1;
@@ -282,16 +365,16 @@ theGame.Game.prototype =
 			this.dude3.animations.play('still',10, true);
 			this.middle = true;
 		}
+		if(this.middle == true)
+		{
+			this.bubble = this.add.sprite(this.world.width*0.2, this.world.height*0.2, 'request');
+			
+		}
 		if(this.redCorrect == true)
 		{
 			this.dude3.x +=0.5;
 			this.middle = false;
 		}
-	},
-	
-	render: function()
-	{
-	
 	},
 
     updateCounter: function() 
@@ -325,31 +408,34 @@ theGame.Game.prototype =
 		else 
 		{
 			this.timer--;
-			this.timerText.setText('Timer: ' + this.timer); // print timer number
+			//this.timerText.setText('Timer: ' + this.timer); // print timer number
 		}
     },
 	
-	redclick: function(sprite, pointer)
+	redclick: function()
 	{
 		if (this.red.events.onInputDown)
-            {
-                this.red.scale.setTo(1.2,1.2);
+			{
+				this.red.scale.setTo(1.2,1.2);
 				if(this.redcustomer!=null)
 				{
 					this.TheDestroyer(this.redcustomer,this.red);
 				}
 				this.blue.scale.setTo(1,1);
 				this.green.scale.setTo(1,1);
-            }
-            else if(this.red.events.onInputUp)
-            {
+				this.yellowGem.scale.setTo(1,1);
+				this.cyanGem.scale.setTo(1,1);
+			}
+			else if(this.red.events.onInputUp)
+			{
 				this.red.scale.setTo(1,1);
 				this.result.kill();
-            }
+			}
 	},
 
 	blueclick: function()
 	{
+		
 		 if (this.blue.events.onInputDown)
             {
                 this.blue.scale.setTo(1.2,1.2);
@@ -359,8 +445,10 @@ theGame.Game.prototype =
 				}
 				this.red.scale.setTo(1,1);
 				this.green.scale.setTo(1,1);
+				this.yellowGem.scale.setTo(1,1);
+				this.cyanGem.scale.setTo(1,1);
             }
-            else if(this.red.events.onInputUp)
+            else if(this.blue.events.onInputUp)
             {    
                  this.blue.scale.setTo(1,1);
 				 this.result.kill();
@@ -378,12 +466,57 @@ theGame.Game.prototype =
 				}
 				this.red.scale.setTo(1,1); // return gem original size
 				this.blue.scale.setTo(1,1);
+				this.yellowGem.scale.setTo(1,1);
+				this.cyanGem.scale.setTo(1,1);
             }
             else if(this.green.events.onInputUp)
             {    
                 this.green.scale.setTo(1,1);
 				this.result.kill();
             }
+		},
+			
+	yellowclick: function()
+		{
+		 if (this.yellowGem.events.onInputDown)
+            {
+                this.yellowGem.scale.setTo(1.2,1.2);
+				if(this.yellowcustomer!=null)
+				{
+					this.TheDestroyer(this.yellowcustomer,this.green); //destroyer gem
+				}
+				this.red.scale.setTo(1,1); // return gem original size
+				this.blue.scale.setTo(1,1);
+				this.green.scale.setTo(1,1);
+				this.cyanGem.scale.setTo(1,1);
+            }
+            else if(this.yellowGem.events.onInputUp)
+            {    
+                this.yellowGem.scale.setTo(1,1);
+				this.result.kill();
+            }
+		},
+			
+	cyanclick: function()
+		{
+		 if (this.cyanGem.events.onInputDown)
+            {
+                this.cyanGem.scale.setTo(1.2,1.2);
+				if(this.cyancustomer!=null)
+				{
+					this.TheDestroyer(this.cyancustomer,this.green); //destroyer gem
+				}
+				this.red.scale.setTo(1,1); // return gem original size
+				this.blue.scale.setTo(1,1);
+				this.green.scale.setTo(1,1);
+				this.yellowGem.scale.setTo(1,1);
+            }
+            else if(this.cyanGem.events.onInputUp)
+            {    
+                this.cyanGem.scale.setTo(1,1);
+				this.result.kill();
+            }
+			
 		},
 	
 	TheDestroyer: function(sprite,sprite2)
@@ -400,7 +533,7 @@ theGame.Game.prototype =
 			this.moneyText.text = 'Money: ' + this.money;
 			
 			this.CusNum += 1;
-			this.CusNumText.text = 'Customer: ' + this.CusNum;
+			this.CusNumText.text = ' : ' + this.CusNum;
 			
 			this.red.scale.setTo(1,1);
 			this.blue.scale.setTo(1,1);
@@ -418,13 +551,13 @@ theGame.Game.prototype =
 	{
 		if(sprite != null && sprite2 != null)
         {
-            if(sprite == this.redcustomer && sprite2 == this.red )
+            if(sprite == this.redcustomer && sprite2 == this.red ) // sprite is customer  sprite2 is gem
             {
 				this.result = this.add.sprite(this.world.width*0.7, this.world.height*0.3, 'correct');
 				this.result.anchor.set(0.5,0.5);
 				this.redCorrect = true;
 				this.customerArray[2] = true;
-				this.spawnCustomer();
+				this.spawnCustomer(); // call next customer
                 return true;
             }
 			else if(sprite == this.greencustomer && sprite2 == this.green )
--- gameoriginal/Scripts/Preloader.js
+++ gameoriginal/Scripts/Preloader.js
@@ -30,6 +30,9 @@ theGame.Preloader.prototype =
         this.load.image('GameBackGround', 'Assets/images/game.png');
         this.load.image('ToturialBackGround', 'Assets/images/tutorial.png');
         
+		//table
+		this.load.image('gemTable', 'Assets/images/table.png');
+        
         //Button
         this.load.image('startButton', 'Assets/images/Play_button.png');
        // this.load.image('tutorialButton', 'Assets/images/tutorialButton.png');
@@ -39,13 +42,19 @@ theGame.Preloader.prototype =
 		this.load.image('red', 'Assets/images/red.png');
 		this.load.image('blue', 'Assets/images/blue.png');
 		this.load.image('green', 'Assets/images/green.png');
+		this.load.image('yellow', 'Assets/images/yellowGem.png');
+		this.load.image('cyan', 'Assets/images/cyanGem.png');
+		
 		
 		//customer
 		this.load.image('redcustomer', 'Assets/images/redCustomer.png');
 		this.load.image('bluecustomer', 'Assets/images/blueCustomer.png');
 		this.load.image('greenCustomer', 'Assets/images/greenCustomer.png');
 		
+		this.load.image('customerIcon', 'Assets/images/customerNO.png');
+		
 		//this.load.spritesheet('greencustomer', 'Assets/images/greencustomer.png', 350, 355, 5);
+		this.load.spritesheet('dude4', 'Assets/images/dude4.png', 32, 48);
 		this.load.spritesheet('dude3', 'Assets/images/dude3.png', 32, 48);
 		this.load.spritesheet('dude2', 'Assets/images/dude2.png', 32, 48);
 		this.load.spritesheet('dude', 'Assets/images/dude.png', 32, 48);
@@ -54,12 +63,17 @@ theGame.Preloader.prototype =
 		this.load.image('correct', 'Assets/images/Correct.png');
 		this.load.image('wrong', 'Assets/images/Wrong.png');
 		
+		//request bubble
+		this.load.image('request', 'Assets/images/bubble.png');
+		
 		//money
-		this.load.spritesheet('moneyImage', 'Assets/images/money.png', 64, 64);
-//		this.load.image('money0', 'Assets/images/money0.png');
-//		this.load.image('money10', 'Assets/images/money10.png');
-//		this.load.image('money20', 'Assets/images/money20.png');
-//		this.load.image('money30', 'Assets/images/money30.png');
+		//this.load.spritesheet('moneyImage', 'Assets/images/money.png', 64, 64);
+		this.load.image('moneyjar', 'Assets/images/moneyjar.png');
+		this.load.image('coin', 'Assets/images/coin.png');
+		
+		//clock
+		this.load.image('clockskin', 'Assets/images/clockskin.png');
+		this.load.image('clock', 'Assets/images/clock.png');
 		
         //Fade In/Out
         this.load.image('FadeInOut', 'Assets/images/Fade.png');
